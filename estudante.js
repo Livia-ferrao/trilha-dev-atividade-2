7de046b3f5ef966385c98a1fe2989a32a11baf6d
Lívia <livia@email.com>
2022-05-15 21:44:00 -0300

    Imprimindo média do estudante

--- estudante.js
+++ estudante.js
@@ -5,3 +5,15 @@ nota3 = 6.9
 idadeAluno = 20
 
 mediaAluno = (nota1*4 + nota2*3 + nota3*3)/(4+3+3)
+
+console.log(nomeAluno)
+console.log(idadeAluno)
+console.log(mediaAluno)
+
+if (mediaAluno >= 7) {
+    console.log("Aluno aprovado")
+} else if (mediaAluno >= 1) {
+    console.log("Aluno reprovado")
+} else if (mediaAluno === 0) {
+    console.log("Aluno zerou")
+}
